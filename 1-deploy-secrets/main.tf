4cec1600cc0412fe2c2fc7e9b7f3f5fb5b81a974
Denis Astahov <adv4000@gmail.com>
2025-10-08 21:03:11 -0700

    Updated to Ephemeral

--- 1-deploy-secrets/main.tf
+++ 1-deploy-secrets/main.tf
@@ -6,7 +6,7 @@ terraform {
   }
 }
 
-data "sops_file" "secrets" {
+ephemeral "sops_file" "secrets" {
   source_file = "secrets.yml"
 }
 
@@ -17,13 +17,15 @@ resource "aws_secretsmanager_secret" "my_secret" {
 }
 
 resource "aws_secretsmanager_secret_version" "my_secret_version" {
-  secret_id     = aws_secretsmanager_secret.my_secret.id
-  secret_string = jsonencode(yamldecode(data.sops_file.secrets.raw))
+  secret_id                = aws_secretsmanager_secret.my_secret.id
+  secret_string_wo         = jsonencode(yamldecode(ephemeral.sops_file.secrets.raw))
+  secret_string_wo_version = 1
 }
 
 #---Deploy Secret file to AWS SSM Parameter Store-------
 resource "aws_ssm_parameter" "my_secret" {
-  name  = "my-secrets"
-  type  = "SecureString"
-  value = jsonencode(yamldecode(data.sops_file.secrets.raw))
+  name             = "my-secrets"
+  type             = "SecureString"
+  value_wo         = jsonencode(yamldecode(ephemeral.sops_file.secrets.raw))
+  value_wo_version = 1
 }
